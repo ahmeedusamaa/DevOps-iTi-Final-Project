a659ad8c701ad05b0a4bab3d1b8f67347873b7dc
Philopater Mansour <philopater.mansour@gmail.com>
2025-06-08 11:27:18 +0300

    feat(security): configure security groups

---
+++ Terraform/Modules/SecGrp/Bastion.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Allow SSH from your IP"
+  vpc_id      = var.VPC_ID
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "bastion-sg"
+  }
+}
---
+++ Terraform/Modules/SecGrp/NG.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "eks_nodes_sg" {
+  name        = "eks-nodes-sg"
+  description = "Security group for EKS self-managed nodes"
+  vpc_id      = var.VPC_ID
+
+  ingress {
+    description = "Node-to-node communication"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow control plane to reach nodes"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-nodes-sg"
+  }
+}
